0cc673193823e6fee0e16f5753b721b895ce8dca
Pranjul Rathour <pranjalrathore9838@gmail.com>
2025-10-29 16:58:30 +0530

    navbar is ready needs to make chages for more atarctive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,41 +3,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { toggleMode } from '../redux/themseSlice'
-import { useState } from 'react'
 
 
 const Navbar = () => {
   const darkMode = useSelector(state => state.theme.darkMode)
-  const [isOpen, setIsOpen] = useState(false)
 
   const dispatch = useDispatch()
+
+
 
   const changeMode = () =>{
      dispatch(toggleMode())
   }
-  
-  const handleOpen = () =>{
-    setIsOpen(true)
-  }
+
   return (
-    <div>
-    <nav className="bg-transparent text-white px-8 py-4 flex justify-evenly items-center">
+    <nav className="bg-transparent text-white px-8 py-4 md:flex md:justify-evenly items-center justify-between">
       <h1 className="text-2xl font-bold font-serif">Pranjul</h1>
+      <div className='flex flex-wrap items-center justify-between md:justify-evenly'>
 
-      <button className='lg:hidden md:hidden' onClick={handleOpen}>Hamburger</button>
-
-       <ul className="flex space-x-6 font-mono">
+       <ul className = "flex flex-wrap items-center flex-row md:space-x-6 ">
         <li><Link to="/" className="hover:text-black hover:bg-zinc-50 p-2 rounded-lg">Home</Link></li>
         <li><Link to="/about" className="hover:text-black hover:bg-zinc-50 p-2 rounded-lg">About</Link></li>
         <li><Link to="/skill" className="hover:text-black hover:bg-zinc-50 p-2 rounded-lg">Skills</Link></li>
         <li><Link to="/contact" className="hover:text-black hover:bg-zinc-50 p-2 rounded-lg">Contact</Link></li>
       </ul>
-    <button onClick={changeMode} className='text-lg' ><FontAwesomeIcon
+      <div className='flex items-center space-x-10'>
+
+     
+    <button onClick={changeMode} className='text-lg md:ml-11' ><FontAwesomeIcon
         icon={darkMode ? faSun : faMoon} 
         className={darkMode ? "text-yellow-400":"text-black h-4 w-4"} 
-      /></button>
-    </nav>
+        /></button>
+   </div>
     </div>
+    </nav>
   )
 }
 
